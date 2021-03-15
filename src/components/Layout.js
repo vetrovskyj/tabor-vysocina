462e9779bcb335c7b01cf0f89ef2d6e96ad2e0d8
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-03-15 21:45:51 +0100

    css import changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './style.css'
-import { withPrefix } from 'gatsby'
 
 const TemplateWrapper = ({ children }) => {
   return (
@@ -16,7 +15,6 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content="Website for Optika Volyně, which sells glasses and provides visual services"/>
         <meta name="author" content="Jan Větrovský"/>
         <title>Domovská stránka | Optika Volyně</title>
-        <link href="./assets/css/style.css" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"/>
         <link rel="icon" type="image/png" href="./assets/img/logo.svg"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
