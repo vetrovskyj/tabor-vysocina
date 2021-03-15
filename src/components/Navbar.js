27f320996360a83b5243d3c0ebeb1e103b65a762
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-03-15 21:22:50 +0100

    navbar repeared, another deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-
-const header = document.querySelector('.logo-and-nav-container');
-
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props)
