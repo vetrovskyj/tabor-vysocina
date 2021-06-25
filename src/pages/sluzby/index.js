7d359d64dda2873d9274882ea52a890aef6a989e
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-06-25 12:03:00 +0200

    zkus

--- src/pages/sluzby/index.js
+++ src/pages/sluzby/index.js
@@ -7,9 +7,9 @@ import Layout from '../../components/Layout'
 export const ServicesPage = ({}) => { 
   return (
     <Layout>
-       <main className="products-main">
+      <main className="products-main">
         <div className="slider-products">
-          <img className="sliderphoto-products" width={1920} alt="sliderphoto" src="../../img/products-image.jpg" />
+          <img className="sliderphoto-products" width={1920} alt="ilustrační foto - interier optiky" src="../../img/products-image.jpg" />
           <div className="slider-sale-and-text-products">
             <h1 className="slider-sale-products">Nabídka</h1>
             <div className="slider-text-products">
@@ -19,17 +19,22 @@ export const ServicesPage = ({}) => {
         </div>
         <div className="product-cards">
           <section className="product-card" id="optometrie">
-            <img src="../../img/eyewear2_optimised.jpg" alt="photo of glasses" />
-            <h2>Optometrie</h2>
+            <img src="../../img/eyewear2_optimised.jpg" alt="foto přístroje na měření zraku" />
+            <a href="./optometrie/">
+              <h2>Optometrie</h2>
+            </a>
             <ul>
               <li>měření dioptrií kvalifikovaným optometristou</li>
               <li>při nákupu brýlí zdarma</li>
               <li>bez nutnosti objednání</li>
             </ul>
+            <a href="./optometrie/" className="card-btn">více</a>
           </section>
           <section className="product-card" id="bryle">
-            <img src="../../img/eyewear3_optimised.jpg" alt="photo of glasses" />
-            <h2>Brýle</h2>
+            <img src="../../img/eyewear3_optimised.jpg" alt="Foto ženy s brýlemi" />
+            <a href="./bryle/">
+              <h2>Brýle</h2>
+            </a>
             <ul>
               <li>prodej dioptrických obrub</li>
               <li>široký sortiment dioptrických skel</li>
@@ -37,27 +42,45 @@ export const ServicesPage = ({}) => {
               <li>záruční i pozáruční servis</li>
               <li>opravy</li>
             </ul>
+            <a href="./bryle/" className="card-btn">více</a>
           </section>
           <section className="product-card">
-            <img src="../../img/kontaktni-cocky.jpg" alt="photo of glasses" />
-            <h2>Kontaktní čočky</h2>
+            <img src="../../img/kontaktni-cocky.jpg" alt="Foto kontaktních čoček" />
+            <a href="./cocky/">
+              <h2>Kontaktní čočky</h2>
+            </a>
             <ul>
               <li>vstupní vyšetření</li>
               <li>konzultace a výběr optimálních čoček</li>
               <li>aplikace</li>
               <li>nácvik</li>
             </ul>
+            <a href="./cocky/" className="card-btn">více</a>
           </section>
           <section className="product-card" id="doplnky">
-            <img src="../../img/eyewear1_optimised.jpg" alt="photo of glasses" />
-            <h2>Doplňkový prodej</h2>
+            <img src="../../img/eyewear1_optimised.jpg" alt="Foto slunečních brýlí" />
+            <a href="./doplnky/">
+              <h2>Doplňkový prodej</h2>
+            </a>
             <ul>
               <li>sluneční brýle</li>
               <li>pouzdra</li>
               <li>čistící prostředky</li>
               <li>roztoky na kontaktní čočky</li>
             </ul>
+            <a href="./doplnky/" className="card-btn">více</a>
           </section>
+        </div>
+        <div className="brands-showcase">
+          <div className="brand-showcase-image">
+            <img src="../../img/converse-eyewear-logo-white.png" alt="converse-eyewear-logo" />
+          </div>
+          <div className="brand-showcase-image">
+            <img src="../../img/bettybarclay_white.png" alt="bettybarclay-logo" />
+          </div>
+          <div className="brand-showcase-image">
+            <img src="../../img/crocs-white.png" alt="crocs-logo" />
+          </div>
         </div>
         <div className="info-footer-container">
           <div className="info-footer">
@@ -76,10 +99,10 @@ export const ServicesPage = ({}) => {
               </div>
             </div>
             <div className="aditional-links">
-              <a href="#">Obchodní podmínky</a>
-              <a href="#">Autorská práva</a>
+              <a href="../cookies/">Používání cookies</a>
+              <a href="../autorska-prava/">Autorská práva</a>
             </div>
-            <a href="https://www.facebook.com/O%C4%8Dn%C3%AD-optika-Volyn%C4%9B-191635897936682/"><img className="fb-icon" alt="facebook-icon" src="../../img/facebook-icon.png" /></a>
+            <a href="https://www.facebook.com/O%C4%8Dn%C3%AD-optika-Volyn%C4%9B-191635897936682/" target="_blank"><img className="fb-icon" alt="facebook-icon" src="../../img/facebook-icon.png" /></a>
           </div>
         </div>
       </main>
