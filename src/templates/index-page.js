72ab96f08ceea0ab210c0901dd5428d87a1bd487
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-07-13 01:16:22 +0200

    banner at hauptpage and disapperaing text

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import Helmet from "react-helmet"
 import { withPrefix } from "gatsby"
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import SaleBanner from '../components/SaleBanner'
 
 import Layout from '../components/Layout'
 
@@ -12,14 +13,15 @@ export const IndexPageTemplate = ({
   uvodni_foto,
   section,
 }) => (
-   <main className="homepage-main">
-   <div className="slider">
-     <div className="sliderphoto" onLoad="sliderFunc">
-      <img src={"../img/"+uvodni_foto.obrazek1.relativePath} className="photo" width={1920} alt="sliderphoto" />
-        <img src={"../img/"+uvodni_foto.obrazek2.relativePath} className="photo" width={1920} alt="sliderphoto" />
-        <img src={"../img/"+uvodni_foto.obrazek3.relativePath} className="photo" width={1920} alt="sliderphoto" />
-     </div>
-     <div className="slider-sale-and-text">
+  <main className="homepage-main">
+    <SaleBanner></SaleBanner>
+    <div className="slider">
+      <div className="sliderphoto" onLoad="sliderFunc">
+        <img src={"../img/" + uvodni_foto.obrazek1.relativePath} className="photo" width={1920} alt="sliderphoto" />
+        <img src={"../img/" + uvodni_foto.obrazek2.relativePath} className="photo" width={1920} alt="sliderphoto" />
+        <img src={"../img/" + uvodni_foto.obrazek3.relativePath} className="photo" width={1920} alt="sliderphoto" />
+      </div>
+      <div className="slider-sale-and-text">
         <p className="slider-sale">{uvodni_text.firstRow}</p>
         <div className="slider-text">
           <p className="slider-text-sale">{uvodni_text.secondRow} <br />{uvodni_text.thirdRow} <span className="slider-text-brand">{uvodni_text.coloredText}</span></p>
@@ -30,56 +32,56 @@ export const IndexPageTemplate = ({
           <div className="rectangle" />
         </div>
       </div>
-   </div>
-   <div className="icons">
-     <a href="./sluzby/optometrie/" className="icon-container">
-       <h2>Optometrie</h2>
-       <img alt="optomerty-icon" src="../img/optometry.png" />
-     </a>
-     <a href="./sluzby/bryle/" className="icon-container">
-       <h2>Brýle</h2>
-       <img alt="glasses-icon" src="../img/glasses.png" />
-     </a>
-     <a href="./sluzby/doplnky/" className="icon-container">
-       <h2>Doplňky</h2>
-       <img alt="accessories-icon" src="../img/accessories.png" />
-     </a>
-   </div>
-   <div className="bottom-section">
+    </div>
+    <div className="icons">
+      <a href="./sluzby/optometrie/" className="icon-container">
+        <h2>Optometrie</h2>
+        <img alt="optomerty-icon" src="../img/optometry.png" />
+      </a>
+      <a href="./sluzby/bryle/" className="icon-container">
+        <h2>Brýle</h2>
+        <img alt="glasses-icon" src="../img/glasses.png" />
+      </a>
+      <a href="./sluzby/doplnky/" className="icon-container">
+        <h2>Doplňky</h2>
+        <img alt="accessories-icon" src="../img/accessories.png" />
+      </a>
+    </div>
+    <div className="bottom-section">
       <section>
         <h2>{section.section_heading}</h2>
         <p>{section.section_text}</p>
       </section>
       <PreviewCompatibleImage className="section-photo" imageInfo={section.image1} />
-  </div>
-   <div className="info-footer-container">
-     <div className="info-footer">
-       <div className="info-contacts">
-         <div className="info-contact">
-           <img alt="person-icon" src="../img/person-icon.png" />
-           <p>Bc. Jitka Hronková</p>
-         </div>
-         <div className="info-contact">
-           <img alt="info-icon" src="../img/info-icon.png" />
-           <p>IČO: 45645687</p>
-         </div>
-         <div className="info-contact">
-           <img alt="location-icon" src="../img/location-icon.png" />
-           <p>nám. Svobody 14, Volyně, 387 01</p>
-         </div>
-       </div>
-       <div className="aditional-links">
-         <a href="/cookies/">Používání cookies</a>
-         <a href="/autorska-prava/">Autorská práva</a>
-       </div>
-       <a href="https://www.facebook.com/O%C4%8Dn%C3%AD-optika-Volyn%C4%9B-191635897936682/" target="_blank"><img className="fb-icon" alt="facebook-icon" src="../img/facebook-icon.png" /></a>
-     </div>
-   </div>
-   <Helmet>
+    </div>
+    <div className="info-footer-container">
+      <div className="info-footer">
+        <div className="info-contacts">
+          <div className="info-contact">
+            <img alt="person-icon" src="../img/person-icon.png" />
+            <p>Bc. Jitka Hronková</p>
+          </div>
+          <div className="info-contact">
+            <img alt="info-icon" src="../img/info-icon.png" />
+            <p>IČO: 45645687</p>
+          </div>
+          <div className="info-contact">
+            <img alt="location-icon" src="../img/location-icon.png" />
+            <p>nám. Svobody 14, Volyně, 387 01</p>
+          </div>
+        </div>
+        <div className="aditional-links">
+          <a href="/cookies/">Používání cookies</a>
+          <a href="/autorska-prava/">Autorská práva</a>
+        </div>
+        <a href="https://www.facebook.com/O%C4%8Dn%C3%AD-optika-Volyn%C4%9B-191635897936682/" target="_blank"><img className="fb-icon" alt="facebook-icon" src="../img/facebook-icon.png" /></a>
+      </div>
+    </div>
+    <Helmet>
       <script src={withPrefix('./scripts/slider.js')} type="text/javascript" />
       <script src={withPrefix('./scripts/hamburger.js')} type="text/javascript" />
     </Helmet>
- </main>
+  </main>
 )
 
 IndexPageTemplate.propTypes = {
