c325991defbb1622b7b81b608b20b93a31d14cc3
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-07-15 16:43:37 +0200

    hamburger fix

--- static/scripts/hamburger.js
+++ static/scripts/hamburger.js
@@ -7,16 +7,21 @@ window.addEventListener('DOMContentLoaded', () => {
             document.querySelector('.ham-nav').remove();
         } else {
             const hamNav = document.createElement('div');
+            console.log(hamNav);
             hamNav.className = 'ham-nav';
             header.appendChild(hamNav);
             const navList = document.createElement('div');
             const navListLineFirst = document.createElement('a');
             navListLineFirst.innerText = 'Produkty a služby';
-            navListLineFirst.setAttribute('href', '/sluzby/')
+            navListLineFirst.setAttribute('href', '/optika-volyne/sluzby/');
+            const navListLineSecond = document.createElement('a');
+            navListLineSecond.innerText = 'Reference';
+            navListLineSecond.setAttribute('href', '/optika-volyne/reference/');
             const navListLineThird = document.createElement('a');
             navListLineThird.innerText = 'Kontakt';
-            navListLineThird.setAttribute('href', '/kontakt/')
+            navListLineThird.setAttribute('href', '/optika-volyne/kontakt/')
             navList.appendChild(navListLineFirst);
+            navList.appendChild(navListLineSecond);
             navList.appendChild(navListLineThird);
             hamNav.appendChild(navList);
         };
