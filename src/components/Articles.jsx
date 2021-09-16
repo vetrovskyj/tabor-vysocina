afee36a549070e59b15b3251400a1ff959774f9e
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-09-16 16:32:13 +0200

    naposled

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -35,16 +35,6 @@ class Articles extends React.Component {
                 }`}
               >
                 <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="thumbnail-image">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `náhledový obrázek ke článku ${post.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
                   <p className="article-title-container">
                     <Link
                       className="article-title"
