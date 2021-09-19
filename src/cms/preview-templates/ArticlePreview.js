791affa285d1333b82882f0a21ae957c61754b7f
Jan Větrovský <vetrovsky.jan@gmail.com>
2021-09-20 01:24:33 +0200

    gonna revert after this

--- src/cms/preview-templates/ArticlePreview.js
+++ src/cms/preview-templates/ArticlePreview.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Articles, ArticlesTemplate } from '../../templates/article-post'
+import { ArticlesTemplate } from '../../templates/article-post'
 
 const ArticlePreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
-  return (
-    <ArticlesTemplate
-      content={widgetFor('body')}
-      description={entry.getIn(['data', 'description'])}
-      tags={tags && tags.toJS()}
-      title={entry.getIn(['data', 'title'])}
-    />
-  )
+    const tags = entry.getIn(['data', 'tags'])
+    return ( <
+        ArticlesTemplate content = { widgetFor('body') }
+        description = { entry.getIn(['data', 'description']) }
+        tags = { tags && tags.toJS() }
+        title = { entry.getIn(['data', 'title']) }
+        />
+    )
 }
 
 ArticlePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    widgetFor: PropTypes.func,
 }
 
 export default ArticlePreview
