7f12a64e8757506e2f55df8ff3701fb9b809df24
Jan Větrovský <vetrovsky.jan@gmail.com>
2023-04-02 00:56:22 +0200

    aktuality fixed

---
+++ src/pages/aktuality/2022-06-26-mame-novy-web/index.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import { Helmet } from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../../../components/Layout";
+import Content, { HTMLContent } from "../../../components/Content";
+
+export const ArticlesTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <section className="article-post-body">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div>
+            <h1 className="article-post-heading">{title}</h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div className="tags">
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map((tag) => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ArticlesTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+};
+
+const ArticlePost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <ArticlesTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Optika Volyně">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+      />
+      <div className="footer-to-bottom"></div>
+    </Layout>
+  );
+};
+
+ArticlePost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default ArticlePost;
+
+export const pageQuery = graphql`
+  query ArticlePostIDFilter {
+    markdownRemark(id: { eq: "2bd679c2-bf4e-5138-b930-10bc72445660" }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "D. MMMM YYYY", locale: "cs")
+        title
+        description
+        tags
+      }
+    }
+  }
+`;
